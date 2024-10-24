25e807e4165119bf9d23dd4ed31cab2fbf7ce959
letty23244 <denladunamis@gmail.com>
2024-10-24 11:10:36 +0300

    new change

--- Cohort4PythonTest/question3.py
+++ Cohort4PythonTest/question3.py
@@ -1,7 +1,7 @@
 # Question 3(i)
 #  Write a Python program that prompts a user to enter numbers. The process will repeat until
 #  the user enters 0. Finally, the program prints sum of the numbers entered by the user.
-number = int(input("Enter a given number: "))
+# number = int(input("Enter a given number: "))
 run = 1
 sum = 0
 
@@ -15,5 +15,6 @@ print(sum)
 # Question 3(ii)
 # Write a Python program to print all the numbers from 1 to 100 that are not divisible by 2
 
+print(1 ,101 // 2) 
     
     
